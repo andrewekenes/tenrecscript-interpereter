9892155117c835141760ed3de1c51966d76a6fda
Andrew Ekenes <84550532+andrewekenes@users.noreply.github.com>
2026-05-11 17:08:33 -0700

    Delete TenrecScript.py

--- TenrecScript.py
+++
@@ -1,9 +0,0 @@
-print("TenrecScript shell")
-print("created by Andrew Ekenes")
-print()
-while True:
-    code = input()
-    if "log" in code:
-        print(">"+code.replace("log", ""))
-    else:
-        print("Syntax error!")
